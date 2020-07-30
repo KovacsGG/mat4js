dd2bee52e782b206550634d355f3180c365a92ac
Kovács Gellért Gábor <kovacsgellertgabor@gmail.com>
2020-07-30 20:39:28 +0200

    Refactor array handling to move num array specific code to its case

--- readmat.js
+++ readmat.js
@@ -120,10 +120,10 @@ function readMat(data) {
 				}
 				reader += nameArr.length;
 
-				var realArr = {};
+				var arrData;
 				switch (mxClass) {
 					case 1: // Cell array
-						realArr.data = [];
+						arrData = {data: []};
 						elemNum = 0;
 						for (var i = 0; i < dim.data.length; i++) {
 							if (!i) {
@@ -134,7 +134,7 @@ function readMat(data) {
 						}
 						for (var i = 0; i < elemNum; i++) {
 							var subArr = readDataElem(data, reader);
-							realArr.data.push(subArr.data);
+							arrData.data.push(subArr.data);
 							reader += subArr.length;
 						}
 						break;
@@ -143,6 +143,7 @@ function readMat(data) {
 					case 3: // Object
 						throw new BadFormatException(index, "Array's type is 3, 'mxOBJECT_CLASS' (unsupported)");
 					case 5: // Sparse array
+
 						throw new BadFormatException(index, "Array's type is 5, 'mxSPARSE_CLASS' (unsupported)");
 					case 4: // Character array
 					case 6: // Double precision array
@@ -153,8 +154,20 @@ function readMat(data) {
 					case 11: // 16-bit, unsigned integer
 					case 12: // 32-bit, signed integer
 					case 13: // 32-bit, unsigned integer
-						realArr = readDataElem(data, reader);
-						reader += realArr.length;
+						if (realFlag) {
+							var realArr = readDataElem(data, reader);
+							reader += realArr.length;
+							var imgArr = readDataElem(data, reader);
+							reader += imgArr.length;
+
+							arrData = [];
+							for (var i = 0; i < imgArr.data.length; i++) {
+								arrData.push({ r: realArr.data[i], i: imgArr.data[i] });
+							}
+						} else {
+							arrData = readDataElem(data, reader);
+							reader += arrData.length;
+						}
 						break;
 					case 14: // 64-bit, signed integer
 						throw new BadFormatException(index, "Array's type is 14, 'mxINT64_CLASS' (unsupported)");
@@ -189,18 +202,7 @@ function readMat(data) {
 					return rec(size, d);
 				}
 
-				if (realFlag) {
-					var imgArr = readDataElem(data, reader);
-					reader += imgArr.length;
-
-					var numArr = [];
-					for (var i = 0; i < imgArr.data.length; i++) {
-						numArr.push({ r: realArr.data[i], i: imgArr.data[i] });
-					}
-					var arrData = iterateN(dim.data, numArr);
-				} else {
-					var arrData = iterateN(dim.data, realArr.data);
-				}
+				arrData = iterateN(dim.data, arrData.data);
 
 				read.name = name;
 				read.data = arrData;
