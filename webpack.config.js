d517639708a2346f4268920cca52fcd1f61c1ccb
Seairth Jacobs <sjacobs@wrsystems.com>
2022-06-07 16:49:52 -0400

    Added webpack dev dependencies to package.json

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const config = require("./package.json");
 
 module.exports = {
+  mode: 'production',
   entry: path.resolve(__dirname, config.main),  // entry file to all our js modules
   output: {
     path: __dirname,  // path to output files
