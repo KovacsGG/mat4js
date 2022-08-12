748a5e2aeb2ead2a724a86ce104f8ab2eea4e32d
Kovács Gellért Gábor <kovacsgellertgabor@gmail.com>
2022-08-12 04:43:39 +0200

    Test cases

---
+++ test/data/create_samples.m
@@ -0,0 +1,46 @@
+samples = ["mxSINGLE", "mxDOUBLE", "mxINT8", "mxUINT8", ...
+    "mxINT16", "mxUINT16", "mxINT32", "mxUINT32", ...
+    "mxINT64", "mxUINT64", "mxCHAR", ...
+    "mxCELL", "mxSTRUCT1", "mxSTRUCT2", "mxSPARSE", ...
+    "LOGICAL"];
+
+mxSINGLE = single([2, 3, 5]);
+mxDOUBLE = [2, 3, 5];
+mxINT8 = int8([2, 3, 5]);
+mxUINT8 = uint8([2, 3, 5]);
+mxINT16 = int16([2, 3, 5]);
+mxUINT16 = uint16([2, 3, 5]);
+mxINT32 = int32([2, 3, 5 + 5i]);
+mxUINT32 = uint32([2, 3, 5]);
+mxINT64 = int64([10000000000000001, 10000000000000002, 10000000000000003]);
+mxUINT64 = uint64([2, 3, 5]);
+
+global mxCHAR;
+mxCHAR = 'abc';
+
+utf8_bytes = unicode2native('UTF-8', "UTF-8");
+utf16_bytes = unicode2native('UTF-16', "UTF-16");
+utf32_bytes = unicode2native('UTF-32', "UTF-32");
+utf8 = native2unicode(utf8_bytes, "UTF-8");
+utf16 = native2unicode(utf16_bytes, "UTF-16");
+utf32 = native2unicode(utf32_bytes, "UTF-32");
+mxCELL = {utf8, utf16, utf32};
+
+mxSTRUCT1.alpha = 'num';
+mxSTRUCT1.num_123 = [1, 2, 3];
+mxSTRUCT2 = struct;
+
+%mxOBJECT
+
+mxSPARSE = sparse(eye(5) * 5);
+
+LOGICAL = logical(mxSPARSE);
+
+for name = samples
+    save(name, name, "-v7", "-nocompression");
+end
+
+save("compressed", "mxDOUBLE", "-v7");
+save("v7-3", "mxDOUBLE", "-v7.3");
+save("v6", "mxDOUBLE", "-v6");
+save("v4", "mxDOUBLE", "-v4");
--- test/read.test.js
+++ test/read.test.js
@@ -1,19 +1,96 @@
 import {read as matread} from "../lib/index.js"
 
-import {readFile} from "node:fs/promises"
+import {readFile, readdir} from "node:fs/promises"
 
-test("miINT8", async () => {
-	const buffer = await readFile("test/data/int_vector.mat")
-	const obj = matread(buffer.buffer)
-	expect(obj).toEqual({
-		"header": "MATLAB 5.0 MAT-file, Platform: GLNXA64, Created on: Fri Jul 31 15:33:27 2020                                        ",
-		"data": {
-			"a": [
-				1,
-				2,
-				3,
-				4,
-			],
-		},
+describe("handling matlab types", () => {
+	const files = {}
+	beforeAll(async () => {
+		const ls = (await readdir("./test/data")).filter(file => /.*\.mat/.test(file))
+		const reads = ls.map(file => readFile("./test/data/" + file))
+		const array = await Promise.all(reads)
+		for (let i = 0; i < array.length; i++) files[ls[i]] = array[i].buffer
+	})
+
+	test("mxINT8", () => {
+		const obj = matread(files["mxINT8.mat"])
+		expect(obj.data).toEqual({mxINT8: [2, 3, 5]})
+	})
+
+	test("mxUINT8", () => {
+		const obj = matread(files["mxUINT8.mat"])
+		expect(obj.data).toEqual({mxUINT8: [2, 3, 5]})
+	})
+	
+	test("mxINT16", () => {
+		const obj = matread(files["mxINT16.mat"])
+		expect(obj.data).toEqual({mxINT16: [2, 3, 5]})
+	})
+	
+	test("mxUINT16", () => {
+		const obj = matread(files["mxUINT16.mat"])
+		expect(obj.data).toEqual({mxUINT16: [2, 3, 5]})
+	})
+	
+	test("mxINT32", () => {
+		const obj = matread(files["mxINT32.mat"])
+		expect(obj.data).toEqual({mxINT32: [{r: 2, i: 0}, {r: 3, i: 0}, {r: 5, i: 5}]})
+	})
+	
+	test("mxUINT32", () => {
+		const obj = matread(files["mxUINT32.mat"])
+		expect(obj.data).toEqual({mxUINT32: [2, 3, 5]})
+	})
+	
+	test("mxINT64", () => {
+		const obj = matread(files["mxINT64.mat"])
+		expect(obj.data).toEqual({mxINT64: [10000000000000001n, 10000000000000002n, 10000000000000003n]})
+	})
+	
+	test("mxUINT64", () => {
+		const obj = matread(files["mxUINT64.mat"])
+		expect(obj.data).toEqual({mxUINT64: [2, 3, 5]})
+	})
+	
+	test("mxSINGLE", () => {
+		const obj = matread(files["mxSINGLE.mat"])
+		expect(obj.data).toEqual({mxSINGLE: [2, 3, 5]})
+	})
+	
+	test("mxDOUBLE", () => {
+		const obj = matread(files["mxDOUBLE.mat"])
+		expect(obj.data).toEqual({mxDOUBLE: [2, 3, 5]})
+	})
+
+	test("mxCHAR", () => {
+		const obj = matread(files["mxCHAR.mat"])
+		expect(obj.data).toEqual({mxCHAR: "abc"})
+	})
+
+	test("mxCELL", () => {
+		const obj = matread(files["mxCELL.mat"])
+		expect(obj.data).toEqual({mxCELL: ["UTF-8", "UTF-16", "UTF-32"]})
+	})
+
+	test("mxSTRUCT1", () => {
+		const obj = matread(files["mxSTRUCT1.mat"])
+		expect(obj.data).toEqual({mxSTRUCT1: {alpha: "num", num_123: [1, 2, 3]}})
+	})
+
+	test("mxSTRUCT2", () => {
+		const obj = matread(files["mxSTRUCT2.mat"])
+		expect(obj.data).toEqual({mxSTRUCT2: {}})
+	})
+
+	test("mxSPARSE", () => {
+		const obj = matread(files["mxSPARSE.mat"])
+		expect(obj.data).toEqual(
+			{mxSPARSE: {x: 5, y:5, nz: [
+				{x:0, y:0, v:5},
+				{x:1, y:1, v:5},
+				{x:2, y:2, v:5},
+				{x:3, y:3, v:5},
+				{x:4, y:4, v:5}
+			]}}
+		)
 	})
 })
